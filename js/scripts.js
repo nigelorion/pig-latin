3d6789014b035b9636e195881b3fb070b78c4fff
Jacob Hixon and Nigel Burnett <jacob@>
2016-10-26 11:14:33 -0700

    Add logic to recognize a vowel.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
-
+// ------ Business Logic -------
 var regexp = /^[a-zA-Z]+$/;
+var vowels = ["a", "e", "i", "o", "u"]
+
 function emptyCheck (userInput) {
 
   if (!userInput || !userInput.match(regexp)) {
@@ -8,29 +10,22 @@ function emptyCheck (userInput) {
   }
 };
 
-// function lettersCheck (userInput) {
-//   if (!userInput.match(regexp)) {
-//     alert("No number or symbols please!")
-//     return false;
-//   }
-// };
 
-
-// function vowelChecker (userInput) {
-//
-//   if ()
-//
-// };
-
+// --------User Int Logic -----------
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
 
     var userInput = $("#text-input").val();
     console.log(userInput);
-
-
     emptyCheck(userInput);
+
+    vowels.forEach(function(vowel) {
+      if (userInput === vowel) {
+        alert("This is a vowel!")
+      }
+
+    });
 
   });
 });
